624b4540902569779d1c512cf171a0ece7829abe
jack-njenga <jacknjenga99@gmail.com>
2023-10-08 14:27:46 +0300

    place filter by search

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,107 @@
+$(document).ready(function () {
+  const currAmenities = {};
+
+  $("input[type='checkbox']").on('change', function () {
+    if (this.checked) {
+      currAmenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete currAmenities[$(this).data('id')];
+    }
+    const amenities = Object.values(currAmenities);
+    $('div.amenities > h4').text(amenities.join(', '));
+  });
+  $.get("http://127.0.0.1:5001/api/v1/status/", function (data) {
+    if (data.status === "OK") {
+      $("div#api_status").addClass("available");
+
+      $.ajax({
+	      type: "POST",
+	      url: "http://127.0.0.1:5001/api/v1/places_search/",
+	      data: "{}",
+	      contentType: "application/json",
+	      dataType: "json",
+	      success: function (data) {
+		      // alert(Object.keys(data[0]));
+		      for (let i = 0; i < data.length; i++) {
+			      let place = data[i];
+			      // alert(place.name);
+			      let article = $("<article></article>");
+			      let placeHtml = `
+			      <div class="headline">
+			      	<h2>${place.name}</h2>
+				<div class="price_by_night">
+				  <p>$${place.price_by_night}</p>
+				</div>
+			      </div>
+			      <div class="information">
+			      	<div class="max_guest">
+				  <div class="guest_icon"></div>
+				  <p>${place.max_guest} Guests</p>
+				</div>
+			      <div class="number_rooms">
+			        <div class="bed_icon"></div>
+				  <p>${place.number_rooms} Bedroom</p>
+			      </div>
+			      <div class="number_bathrooms">
+			      	<div class="bath_icon"></div>
+				  <p>${place.number_bathrooms} Bathroom</p>
+				</div>
+			      </div>
+			      <div class="user"><b>Owner</b>: ${place.usernames}</div>
+			      <div class="description">${place.description}</div>
+			      `;
+			      article.html(placeHtml);
+			      $(".places").append(article);
+		      }
+	      },
+	      error: function (xhr, status, error) {
+		      alert("error");
+	      }
+      });
+      $("button:contains('Search')").click(function () {
+	      $(".places > article").remove();
+	      $.ajax({
+		      type: "POST",
+		      url: "http://127.0.0.1:5001/api/v1/places_search",
+		      data: JSON.stringify({'amenities': Object.keys(currAmenities)}),
+		      dataType: "json",
+		      contentType: "application/json",
+		      success: function (data) {
+			      for (let i = 0; i < data.length; i++) {
+				      let place = data[i];
+				      let article = $("<article></article>");
+
+				      let placeHtml = `
+				      <div class="title_box">
+				        <h2>${place.name}</h2>
+					<div class="price_by_night">
+					  $${place.price_by_night}
+					</div>
+                              	      </div>
+                              	      <div class="information">
+                                         <div class="max_guest">
+					   ${place.max_guest} Guests
+                                	 </div>
+                              		<div class="number_rooms">
+                                	  ${place.number_rooms} Bedroom
+                              		 </div>
+                              		 <div class="number_bathrooms">
+                                	   ${place.number_bathrooms} Bathroom
+                                	 </div>
+                              		 </div>
+                              		 <div class="user"><b>Owner</b>: ${place.usernames}</div>
+                              		 <div class="description">${place.description}</div
+				      `;
+				      article.html(placeHtml);
+				      $(".places").append(article);
+			      }
+		      },
+		      error: function (xhr, status, error) {alert("error");}
+	      });
+      });
+    } else {
+      // alert("NOP");
+      $('div#api_status').removeClass('available');
+    }
+  });
+});
